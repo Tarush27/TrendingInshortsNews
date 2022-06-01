7cf057ff07d6d41bbe26d9ddf10d9ada0ce18c9a
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-06-01 17:31:30 +0530

    added params such as image, title, author, content for updating view holder items.

--- app/src/main/java/newsModel/NewsModel.kt
+++ app/src/main/java/newsModel/NewsModel.kt
@@ -1,3 +1,8 @@
 package newsModel
 
-data class NewsModel()
+data class NewsModel(
+    val newsImage: Int?,
+    val newsTitle: String?,
+    val newsAuthor: String?,
+    val newsContent: String?
+)
