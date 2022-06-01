bf9eb8927e59181daca9cf3d41c330b8e91df574
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-06-01 18:11:56 +0530

    initialized array list of news model along with content.

--- app/src/main/java/news_model_list_singleton/NewsModelList.kt
+++ app/src/main/java/news_model_list_singleton/NewsModelList.kt
@@ -1,4 +1,32 @@
 package news_model_list_singleton
 
+import newsModel.NewsModel
+
 object NewsModelList {
+    val newsList = arrayListOf(
+        NewsModel(
+            newsImage = "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2021/09_sep/22_wed/img_1632280365719_940.jpg?",
+            newsTitle = "PBKS Wins",
+            newsAuthor = "Tarusb Kaistha",
+            newsContent = """
+                Ex-India captain Sunil Gavaskar criticised Punjab Kings for not playing Chris Gayle against Rajasthan Royals on the occasion of the West Indian's 42nd birthday. \"He has dominated every single T20 league and you drop him for this game on his birthday makes zero sense,\" Gavaskar said. Meanwhile, Kevin Pietersen said, \"I can't understand PBKS' thinking at all.
+            """.trimIndent()
+        ),
+        NewsModel(
+            newsImage = "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2021/09_sep/22_wed/img_1632280365719_940.jpg?",
+            newsTitle = "PBKS Wins",
+            newsAuthor = "Tarusb Kaistha",
+            newsContent = """
+                Ex-India captain Sunil Gavaskar criticised Punjab Kings for not playing Chris Gayle against Rajasthan Royals on the occasion of the West Indian's 42nd birthday. \"He has dominated every single T20 league and you drop him for this game on his birthday makes zero sense,\" Gavaskar said. Meanwhile, Kevin Pietersen said, \"I can't understand PBKS' thinking at all.
+            """.trimIndent()
+        ),
+        NewsModel(
+            newsImage = "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2021/09_sep/22_wed/img_1632280365719_940.jpg?",
+            newsTitle = "PBKS Wins",
+            newsAuthor = "Tarusb Kaistha",
+            newsContent = """
+                Ex-India captain Sunil Gavaskar criticised Punjab Kings for not playing Chris Gayle against Rajasthan Royals on the occasion of the West Indian's 42nd birthday. \"He has dominated every single T20 league and you drop him for this game on his birthday makes zero sense,\" Gavaskar said. Meanwhile, Kevin Pietersen said, \"I can't understand PBKS' thinking at all.
+            """.trimIndent()
+        )
+    )
 }
