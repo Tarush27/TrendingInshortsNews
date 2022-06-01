4aa8f9199792d5d1d10bb1d121d498b92ad069fc
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-06-01 18:20:17 +0530

    created news data object which stores data in news model in array list form through news list object invoked from news model list object class.

--- app/src/main/java/newsAdapter/NewsAdapter.kt
+++ app/src/main/java/newsAdapter/NewsAdapter.kt
@@ -1,4 +1,25 @@
 package newsAdapter
 
-class NewsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.trendinginshortsnews.R
+import newsModel.NewsModel
+import newsViewHolder.NewsViewHolder
+import news_model_list_singleton.NewsModelList
+
+class NewsAdapter : RecyclerView.Adapter<NewsViewHolder>() {
+
+    private val newsData: ArrayList<NewsModel> = NewsModelList.newsList
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val newsViewHolder = LayoutInflater.from(parent.context)
+            .inflate(R.layout.single_inshorts_news, parent, false)
+        return NewsViewHolder(newsViewHolder)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = newsData.size
 }
