b22d134b39eb58907f9b53634b521725f16129d9
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-06-08 13:41:10 +0530

    initialized recycler view and set adapter to show news content.

--- app/src/main/java/com/example/trendinginshortsnews/InshortsNewsMainActivity.kt
+++ app/src/main/java/com/example/trendinginshortsnews/InshortsNewsMainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.trendinginshortsnews
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import newsAdapter.NewsAdapter
 
 class InshortsNewsMainActivity : AppCompatActivity() {
+
+    private val newsRecyclerView: RecyclerView
+        get() = findViewById(R.id.news_rv)
+    private lateinit var newsAdapter: NewsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.inshorts_news_main_activity)
+        newsRecyclerView.layoutManager = LinearLayoutManager(this)
+        newsAdapter = NewsAdapter()
+        newsRecyclerView.adapter = newsAdapter
     }
 }
