bad9cd864c7b4471cff343ac1e43fd44ef81ed82
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-07-04 15:00:25 +0530

    removed un-necessary return actual news function.

--- app/src/main/java/network/InshortsNewsResponse.kt
+++ app/src/main/java/network/InshortsNewsResponse.kt
@@ -1,7 +1,6 @@
 package network
 
 import com.google.gson.annotations.SerializedName
-import newsModel.NewsModel
 
 data class NewsResponse(
     @SerializedName("category") var newsCategory: String?,
@@ -9,18 +8,6 @@ data class NewsResponse(
 ) {
 
 
-    fun returnActualNews(): List<NewsModel> {
-        val newsModelList: List<NewsModel> = newsList.map {
-            NewsModel(
-                category = newsCategory,
-                newsAuthor = it.author,
-                newsContent = it.content,
-                newsImage = it.imageUrl,
-                newsTitle = it.title
-            )
-        }
-        return newsModelList
-    }
 }
 
 data class NewsData(
